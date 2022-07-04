aa4c9edbc60c2c9d8907afad03ad4b821b2b2212
Olawale <olapetanwale@outlook.com>
2022-07-04 11:14:59 +0100

    Set up create react app

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class TodoContainer extends Component {
+    render() {
+        return(
+            <div>
+                <h1> Hello from Create React App</h1>
+                <p>I am in a React Component</p>
+            </div>
+        )
+    }
+}
+export default TodoContainer;
